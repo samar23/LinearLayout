09859243496e41b916f7e31024c3a3169f2c08b0
Samarinder Singh <samarindersingh@Samarinders-MacBook-Air.local>
2017-04-07 00:48:35 +0530

    first commit

---
+++ app/src/main/java/learningandroid/linearlayoutexample/VerticalLayoutActivity.java
@@ -0,0 +1,13 @@
+package learningandroid.linearlayoutexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerticalLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vertical_layout);
+    }
+}
